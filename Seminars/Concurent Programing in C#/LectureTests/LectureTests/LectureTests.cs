fb894a1d60793845d67b675bb000ff4dfe8ba1bd
Jazastry <yogib@mail.bg>
2014-08-16 11:30:54 +0300

    Added C#Exam & Seminars folders.

---
+++ Seminars/Concurent Programing in C#/LectureTests/LectureTests/LectureTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureTests
+{
+	class LectureTests
+	{		
+		static void Main(string[] args)
+		{
+			int tryMeBe = 0;
+
+			for (int i = 0; i < 10; i++)
+			{
+				tryMeBe++;
+				Console.WriteLine(tryMeBe);
+			}
+
+		}
+	}
+}
---
+++ Seminars/Concurent Programing in C#/LectureTests/SwitchForeachExercise/SwitchForeachExercise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class SwitchForeachExercise
+{
+
+	static void Main()
+	{
+		string inputDayofWeek = Console.ReadLine();
+
+		double[] quantiy = new double[3];
+
+		string[] inputProduct = new string[3];
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				
+			}
+		}
+	}
+}
+
