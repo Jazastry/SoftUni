136c2921042e1b3485e7d2a0700d348dbb419faa
Jazastry <yogib@mail.bg>
2014-11-14 17:21:20 +0200

    Added Homework-Defensive-Programming.

---
+++ Cuality-Programming-Code/Deffensive-Programming/Homework-Defensive-Programming/Exceptions/Exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Exceptions_Homework;
+
+class Exceptions
+{
+    static void Main()
+    {
+        var substr = Extensions.Subsequence("Hello!".ToCharArray(), 2, 5);
+        Console.WriteLine(substr);
+
+        var subarr = Extensions.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 2);
+        Console.WriteLine(String.Join(" ", subarr));
+
+        var allarr = Extensions.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 4);
+        Console.WriteLine(String.Join(" ", allarr));
+
+        var emptyarr = Extensions.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 0);
+        Console.WriteLine(String.Join(" ", emptyarr));
+
+        Console.WriteLine(Extensions.ExtractEnding("I love C#", 2));
+        Console.WriteLine(Extensions.ExtractEnding("Nakov", 4));
+        Console.WriteLine(Extensions.ExtractEnding("beer", 4));
+        Console.WriteLine(Extensions.ExtractEnding("Hi", 100));
+
+        Extensions.CheckPrime(23);
+        Extensions.CheckPrime(33);
+
+        List<Exam> peterExams = new List<Exam>()
+        {
+            new SimpleMathExam(2),
+            new CSharpExam(55),
+            new CSharpExam(100),
+            new SimpleMathExam(1),
+            new CSharpExam(0),
+        };
+        Student peter = new Student("Peter", "Petrov", peterExams);
+        double peterAverageResult = peter.CalcAverageExamResultInPercents();
+        Console.WriteLine("Average results = {0:p0}", peterAverageResult);
+    }
+}
---
+++ Cuality-Programming-Code/Deffensive-Programming/Homework-Defensive-Programming/Exceptions/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exceptions_Homework
+{
+    class Extensions
+    {
+        public static T[] Subsequence<T>(T[] arr, int startIndex, int count)
+        {
+            if ((arr.Length - 1 < startIndex) || (0 > startIndex))
+            {
+                throw new IndexOutOfRangeException(String.Format("Invalid Index {0}", startIndex));
+            }
+            else if (arr.Length - startIndex < count)
+            {
+                throw new ArgumentOutOfRangeException("count", "Count is Out of the string length !");
+            }
+            List<T> result = new List<T>();
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                result.Add(arr[i]);
+            }
+            return result.ToArray();
+        }
+
+        public static string ExtractEnding(string str, int count)
+        {
+            if (count > str.Length)
+            {
+                throw new ArgumentOutOfRangeException("count", "Count is out of string length!");
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = str.Length - count; i < str.Length; i++)
+            {
+                result.Append(str[i]);
+            }
+            return result.ToString();
+        }
+
+        public static void CheckPrime(int number)
+        {
+            for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number%divisor == 0)
+                {
+                    Console.WriteLine("The number is not prime!");
+                }
+                else
+                {
+                    Console.WriteLine("The number is prime !");
+                }
+            }
+        }
+    }
+}
