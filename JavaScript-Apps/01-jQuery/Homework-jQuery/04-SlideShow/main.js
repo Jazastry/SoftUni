aee9a3c693b06c8113394948eaf1990743ee8ba9
Jazastry <yogib@mail.bg>
2014-11-24 18:49:18 +0200

    Added last exercise.

--- JavaScript-Apps/01-jQuery/Homework-jQuery/04-SlideShow/main.js
+++ JavaScript-Apps/01-jQuery/Homework-jQuery/04-SlideShow/main.js
@@ -1,7 +1,25 @@
+var slider = $('.slider');
+for (var i = 0; i < slider.length; i++) {
+	if (i !== 1) {
+		$(slider[i]).css('display','none');
+	}
+}
+
 $("button").click(function (e) {
 	var direction;
 	direction = e.target.value;
-	console.log(direction);
-	$("#slider1").animate({width:'50'}, 500);
-	 return false;
+	if (direction === 'left') {
+		$(slider[1]).hide("slide", { direction: 'left' }, 1000);
+		$(slider[2]).show("slide", {direction: 'right'}, 1000);
+		$(slider[0]).insertAfter(slider[3]);
+		slider = $('.slider');
+		console.log(slider);
+	} else if (direction === 'right') {
+		$(slider[3]).insertBefore(slider[0]);
+		$(slider[1]).hide("slide", { direction: 'right' }, 1000);
+		$(slider[0]).show("slide", {direction: 'left'}, 1000);
+		slider = $('.slider');
+	}
+
+	 return false;	
 });
