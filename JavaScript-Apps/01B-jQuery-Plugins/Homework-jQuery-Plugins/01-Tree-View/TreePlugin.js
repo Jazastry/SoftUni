3687725bf8e6f32b2914cb37a0b3a554ab7e5236
Jazastry <yogib@mail.bg>
2014-11-24 23:10:07 +0200

    Added 01B-jQuery-Plugins/Homework-jQuery-Plugins.

---
+++ JavaScript-Apps/01B-jQuery-Plugins/Homework-jQuery-Plugins/01-Tree-View/TreePlugin.js
@@ -0,0 +1,18 @@
+(function($){
+	$.fn.threeView = function() {
+		var $this = $(this);
+		$this.on('click', function(e) { 
+			var click = e.target;
+
+			if ($(click).find(':last-child').is(':hidden')) {
+				$(click).find(':hidden').show();
+				return false;
+			} else {
+				$(click).children().children().hide();
+				return false;
+			}
+		});
+
+		return $this;
+	};
+}(jQuery));
